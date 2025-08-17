6af2802dc146993b5d8dd850829efb99bc82dbce
Matthew Pearson <matthewpearson803@gmail.com>
2025-08-17 14:58:04 -0400

    Created color presets for nativewind

--- movie-app/tailwind.config.js
+++ movie-app/tailwind.config.js
@@ -8,8 +8,22 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        accent: "#AB8BFF",
+        light: {
+          100: '#d6c6ff',
+          200: '#a8b5db',
+          300: '#9ca4ab',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23'
+        }
+      }
+    },
   },
-  plugins: [],
+plugins: [],
 }
-
